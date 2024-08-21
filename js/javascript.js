67cf205fa65df8a56aaed54411b3f4518224b906
italoliraa <143004026+Italoliraa@users.noreply.github.com>
2024-08-21 17:16:43 -0300

    Atualizações

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+// var ask = prompt('Digite o seu nome: ')
+
+function calcular(tipo, valor){
+
+    
+
+    if (tipo === 'acao'){
+
+    } else if(tipo === 'valor'){
+
+        document.getElementById('resultado').value += valor 
+    }
+
+}
